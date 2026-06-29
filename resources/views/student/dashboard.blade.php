be4fa4b14f78db1e85acd21ef4e3d0aee5eab55c
GeraldiJari (Red) <assajigeraldi@gmail.com>
2026-06-29 23:33:34 +0700

    Add: Course Count & Course Preview

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -16,6 +16,7 @@
 
     </div>
 
+    {{-- Statistics Card --}}
     <div class="grid md:grid-cols-3 gap-6">
 
         <div class="bg-white rounded-xl shadow-sm p-6">
@@ -25,7 +26,7 @@
             </p>
 
             <h2 class="text-3xl font-bold mt-2">
-                0
+                {{ $courses->count() }}
             </h2>
 
         </div>
@@ -55,6 +56,43 @@
         </div>
 
     </div>
+
+    {{--  Preview Courses --}}
+    <div class="bg-white rounded-xl shadow-sm p-6">
+        <h2 class="text-xl font-bold mb-5">
+            My Courses
+        </h2>
+
+        <div class="space-y-3">
+
+            @forelse($courses as $course)
+
+                <div class="border rounded-lg p-4">
+
+                    <h3 class="font-semibold">
+                        {{ $course->name }}
+                    </h3>
+
+                    <p class="text-sm text-gray-500">
+                        {{ $course->description }}
+                    </p>
+
+                </div>
+
+            @empty
+
+                <p class="text-gray-500">
+
+                    Belum ada course.
+
+                </p>
+
+            @endforelse
+
+        </div>
+
+    </div>
+    
 
 </div>
 
